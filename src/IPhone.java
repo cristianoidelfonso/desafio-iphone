4f2f5a7ea3d568ede0cf5e1ebd5b11485fa8b60e
cristianoidelfonso <cristiano_idelfonso@hotmail.com>
2024-10-05 14:49:56 -0300

    desafio iphone dio

---
+++ src/IPhone.java
@@ -0,0 +1,52 @@
+public class IPhone implements AparelhoTelefonico, NavegadorInternet, ReprodutorMusical {
+    
+    public static void main(String[] args) throws Exception {
+        System.out.println("Desafio Iphone - DIO");
+    }
+
+    @Override
+    public void ligar(String numero) {
+        /** TODO: implements your code */
+    }
+
+    @Override
+    public void atender(String numero) {
+        /** TODO: implements your code */
+    }
+
+    @Override
+    public void iniciarCorreioVoz(String mensagem, String destinatario) {
+        /** TODO: implements your code */
+    }
+
+    @Override
+    public void exibirPagina(String url) {
+        /** TODO: implements your code */
+    }
+
+    @Override
+    public void adicionarNovaAba(String url) {
+        /** TODO: implements your code */
+    }
+
+    @Override
+    public void atualizarPagina() {
+        /** TODO: implements your code */
+    }
+
+    @Override
+    public void tocar() {
+        /** TODO: implements your code */
+    }
+
+    @Override
+    public void pausar() {
+        /** TODO: implements your code */
+    }
+
+    @Override
+    public void selecionarMusica() {
+        /** TODO: implements your code */
+    }
+
+}
---
+++ src/NavegadorInternet.java
@@ -0,0 +1,10 @@
+
+public interface NavegadorInternet {
+
+    public void exibirPagina(String url);
+
+    public void adicionarNovaAba(String url);
+    
+    public void atualizarPagina();
+
+}
